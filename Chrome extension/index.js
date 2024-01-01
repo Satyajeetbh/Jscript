0fb7477c8b9d2bfcdf80bb20aa4bfeafaa1ad6a5
Satyajeetbh <satyjeetsatyam5@gmail.com>
2024-01-01 16:31:48 +0530

    added new directory

---
+++ Chrome extension/index.js
@@ -0,0 +1,8 @@
+const inputBtn=document.getElementById("input-btn")
+let myLeads=[]
+const inputEl=document.getElementById("input-el")
+
+
+inputBtn.addEventListener("click",function(){
+    console.log("Button clicked from addEventListener")
+})
