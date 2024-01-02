640106f3fd489c047da72f114b2cf56b457fdce3
Satyajeetbh <satyjeetsatyam5@gmail.com>
2024-01-02 12:58:12 +0530

    modified the appearance and added manifest.json

--- Chrome extension/index.js
+++ Chrome extension/index.js
@@ -1,11 +1,34 @@
 const inputBtn=document.getElementById("input-btn")
 let myLeads=[]
 const inputEl=document.getElementById("input-el")
+const ulEl=document.getElementById("ul-el")
 
 
 inputBtn.addEventListener("click",function(){
-    myLeads.push(inputEl.value) 
-    for(let i=0;i<myLeads.length;i++){
-        console.log(myLeads[i])
-    }
+    myLeads.push(inputEl.value)
+    inputEl.value=""
+    renderLeads()
+    
+    
 })
+// function renderLeads(){
+//     ulEl.innerHTML= ""//clears the list
+// for(let i=0;i<myLeads.length;i++){
+//     // ulEl.innerHTML+= "<li>"+myLeads[i]+"</li>" one way to it
+//     const li=document.createElement("li")
+//     li.textContent=myLeads[i]
+//     ulEl.append(li)
+// } }
+
+function renderLeads(){
+    let listItems=""
+for(let i=0;i<myLeads.length;i++){
+    // listItems+= "<li>"+"<a href='"+myLeads[i]+"' target='_blank'>" +myLeads[i]+"</a>"+"</li>"
+    listItems+=`<li>
+    <a target="_blank" href='${myLeads[i]}'>
+    ${myLeads[i]}
+     </a>
+    </li>`
+}
+ulEl.innerHTML=listItems
+}
